60209c7ec84d6be8fec7e163a6d97fac5a3b3d85
sujita lama <naresh-lama>
2024-07-19 21:53:38 +0900

    learn syncronous

--- new_js_day_one/js/script.js
+++ new_js_day_one/js/script.js
@@ -179,17 +179,23 @@ const returnArray = a.map(function(elements, index){
     // returnArry chahi value store garne 
     /// retrunArry = [1, 2,3 ,4 5, 6, 7, 8]
 });
+
+const b = [10, 11, 12, 13, 14];
+const retunrBox = b.map(function(boxMap, index){
+  return boxMap + index;
+});
   
+console.log(retunrBox);
 console.log(returnArray);  /// console.log always take under the return
 
 /// filter ---> manipulating an array --> alwasy retrun an array
 
 const population = [
-    {name: "saula karki", age: 21, married: false},
-    {name: "Naresh lama", age: 26, married: true},
-    {name: "Shushma lama", age: 22, married: true},
-    {name: "Sunima lama", age: 21, married: false},
-    {name: "pushpa Karki", age: 21, married: true},
+    {name: "saula karki", age: 29, married: false, city: "nepal"},
+    {name: "Naresh lama", age: 36, married: true, city: "japan"},
+    {name: "Shushma lama", age: 35, married: true, city: "japan"},
+    {name: "Sunima lama", age: 21, married: false, city: "canada"},
+    {name: "pushpa Karki", age: 21, married: true, city: "asutrailia"},
 ];
 
 const marrieddataStore = population.filter((elements) => {
@@ -200,7 +206,160 @@ const unmarrieddataStore = population.filter((elements) => {
     return elements.married === false;
 });
 
+const ageData = population.filter((elements) => {
+  return elements.age >= 30;
+});
+
+const underAage = population.filter((elements) => {
+  return elements.age < 30;
+});
+
+const cityFilter = population.filter((cityArea) => {
+  return cityArea.city == "japan";
+});
+
+
 console.log(marrieddataStore);
 console.log(unmarrieddataStore);
+console.log(ageData);
+console.log(underAage);
+console.log(cityFilter);
+
+/// reduce ---> manipulating an array ---> single value matra return
+// a = [1, 2, 3, 4, 5, 6, 7, 8];
+const singleValue = a.reduce((accumalte, data) => { //accumalte bhaneko euta initial number ho jun call back function ko last ma rakhinchha
+ return accumalte + data; /// data equal sto 1,2,3...8
+}, 0); /// 0 value is for accomalate
+
+/// 0 + 1 = 1 accumulate + data last ma value nai next ma gayera add hudai janchha
+/// 1 + 2 = 3
+/// 3 + 3 = 6
+/// 6 + 4 = 10
+/// 10 + 5 = 15
+/// 15 + 6 = 21
+/// 21 + 7 = 28
+/// 28 + 8 = 66 return value is 36
+
+
+const singleWithindex = a.reduce((accumalte, data, index) => {
+ const total = accumalte + data + index;
+  return total;
+}, 2);
+
+console.log(singleValue);
+console.log(singleWithindex);
+
+const x = [1, 2, 3, 4, 5, 6];
+
+const singleNo = x.reduce((accumalte, reduceNumber) => {
+  const totalReduce = accumalte + reduceNumber;
+  return totalReduce;
+}, 3);
+
+console.log(`this is total of reduce manimulating ${singleNo}`);
+/// forech ---> manipulating array --> doesent retrun an array --> instead retruns single value for each of the elements
+/// array nai linchha but data store chahi array ma gardaina
+
+
+
+// Synchronous and Asynchronous =>
+// Synchronous....>jun way ma javascript palai palo run hunchh teslai synchronous bhaninchha
+// palai palo run huda bich ma time set bhako chha bhane tyo exp 9 sec ko hold gare sakie pachhi tala ko run hunchh
+// jun 9 sec hold bhaye pachhi matrai tala ko run garne lai nai synchronous bhaninchha
+
+// *backend ko data heavy or network problem le garda page load huda
+// java script ko engine bhitra chahi hold bhai rakhchha but tala ko sabai run hune kam hunchha bhane Asynchronous
+
+// sychronous ...>>> hold garaune
+// asynchronous..>>> hold hune chahi bhairakhchha but run hune content chahi run bhairakhchha
+
+// api...>> data include by API 
+// sabai api spead hune hudaina but promise handling garera run garchha
+// promises handling
+
+async() => {
+  await fetch("facebook.com");
+}
+
+console.log("this is already printed");
+
+/// sychronous >> nature ... this code will for everyline to be executed
+/// harek line lai wait garchha kati time lagchha bhanere tyo time nasake samma 
+console.log("Naresh"); /// 0.1sec
+console.log("lama"); /// 5sec
+
+
+/// asynchronous ..>>> harek line line run wait gardaina if kunai lai run huna time lagchha 
+/// tyo chahi bhitra nai run bhairakchha tyo bicha tala ko line chahi run bhayera retrun hunchha
+/// this code doesn`t waits everyline to be executed
+
+/// synchronous nature
+console.log("start");
+
+
+/// asynchronous code
+/// handling promieses  holding gare ni bhitra nai run gara,... pratek lai promises use garnu parchha so yo bhanda ta await wait mostly use hunchha
+/// .then()
+/// .async await >.... async function await nature use garne //// await bhane ko wait garna lai use garne
+
+
+// console.log("Start");
+
+// function wait1(time) {
+//   const start = Date.now();
+
+//   let now = start;
+
+//   while (now - start < time) {
+//     now = Date.now();
+//   }
+
+//   return now;
+// }
+
+// wait1(20000); // holds for 2 sec
+
+// console.log("End");
+
+// asynchronous code
+// handling promises
+// 1. .then()
+// 2. .async await
+
+// .then
+console.log("Start");
+
+function wait(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
+wait(5000).then(function () {
+  console.log("This is Loading");
+});
+
+console.log("End");
+
+// async await
+
+console.log("Start");
+
+function wait(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
+async function Japan() {
+  await wait(6000);
+  console.log("This is Loading");
+}
+
+Japan();
+
+console.log("End");
+
+
 
 
